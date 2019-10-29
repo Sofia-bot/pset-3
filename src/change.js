5ecacb52ac5f4523f6e8d9093acb04319d0ac139
Sofia Regencia <SRegencia@ucvts.tec.nj.us>
2019-10-29 10:10:31 -0400

    initial commit

--- src/change.js
+++ src/change.js
@@ -6,9 +6,17 @@ const MAX = 0.99;
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("Invalid.");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("Invalid.");
 } else {
-    // write your code for making change here
+    let quarters = Math.floor(amount / 0.25);
+    let remainder = amount % 0.25;
+    let dimes = Math.floor(remainder / 0.1);
+    remainder %= 0.1;
+    let nickels = Math.floor(remainder / 0.05);
+    remainder %= 0.05;
+    let pennies = Math.ceil(remainder / 0.01);
+
+    console.log(quarters + " quarters, " + dimes + " dimes, " + nickels + " nickels, " + pennies + " pennies.");
 }

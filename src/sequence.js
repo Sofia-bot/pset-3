b1fcae7a2ac17b4fff24d638fcbe7782e0c69907
Sofia Regencia <sregencia@ucvts.org>
2019-11-06 10:45:29 -0500

    updated sequence

--- src/sequence.js
+++ src/sequence.js
@@ -9,17 +9,23 @@ const number2 = Number(readlineSync.question());
 const number3 = Number(readlineSync.question());
 
 if (number1 < MIN || number1 > MAX || number2 < MIN || number2 > MAX || number3 < MIN || number3 > MAX) {
-  console.log("\nInvalid.");
+    console.log("\nInvalid.");
 } else if (Number.isNaN(number1, number2, number3)) {
-  console.log("\nInvalid.")
+    console.log("\nInvalid.")
 } else {
   if (number1 < number2 && number2 < number3){
     console.log("\nStrictly increasing.");
+  } else if (number1 > number2 && number2 > number3) {
+      console.log("\nStrictly decreasing.");
+  } else if (number1 == number2 && number1 == number3 && number2 == number3) {
+      console.log("\nEqual.");
   } else if (number1 == number2 || number2 == number3) {
-    if (number1 < number3) {
-      console.log("Increasing.");
-    } else if (number1 > number3) {
-      console.log("Decreasing.");
+      if (number1 < number3) {
+        console.log("\nIncreasing.");
+      } else if (number1 > number3) {
+          console.log("\nDecreasing.");
+      }
+    } else {
+      console.log("\nUnordered.");
     }
-  }
 }
